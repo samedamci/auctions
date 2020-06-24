46095dc6d404a6b5136617d499d37b908b9b64a4
samedamci <samedamci@disroot.org>
2020-06-24 22:31:16 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+from flask import Flask, render_template, url_for
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+if "__name__" == "__main__":
+    app.run(debug=True)
