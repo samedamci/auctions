df2e81663fefc51aa73d41d350800fe23a0f3875
samedamci <samedamci@disroot.org>
2020-06-25 11:45:47 +0200

    Add visual feedback to registration

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
 
 from settings import SECRET_KEY
 from forms import Register, Login
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = SECRET_KEY
+app.config["SECRET_KEY"] = SECRET_KEY
 
 
 @app.route("/")
@@ -19,11 +19,16 @@ def about():
     return render_template("about.html")
 
 
-@app.route("/login")
+@app.route("/login", methods=["GET", "POST"])
 def login():
-    return render_template("login.html", form=Login())
+    form = Login()
+    return render_template("login.html", form=form)
 
 
-@app.route("/register")
+@app.route("/register", methods=["GET", "POST"])
 def register():
-    return render_template("register.html", form=Register())
+    form = Register()
+    if form.validate_on_submit():
+        flash(f'Account "{form.username.data}" created successfully!', "success")
+        return redirect(url_for("home"))
+    return render_template("register.html", form=form)
