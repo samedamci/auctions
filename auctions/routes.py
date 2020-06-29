4004eb07e72df3b6140519913b57c8ac5322abe1
samedamci <samedamci@disroot.org>
2020-06-29 12:54:37 +0200

    Add JSON API with auctions

--- auctions/routes.py
+++ auctions/routes.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from flask import render_template, url_for, flash, redirect
+from flask import render_template, url_for, flash, redirect, jsonify, request
 from auctions.forms import Register, Login, AddAuction
 from auctions.models import User, Auction
 from auctions import app, db, bcrypt
@@ -124,3 +124,45 @@ def add_auction():
             flash(f'Auction "{form.title.data}" has been added!', "success")
             return redirect(url_for("auctions"))
         return render_template("add_auction.html", form=form)
+
+
+# API.
+auction_list = []
+auction_count = Auction.query.count()
+for id in range(auction_count):
+    auction_ = Auction.query.get(id + 1)
+    auction_list.append(
+        {
+            "id": id + 1,
+            "title": auction_.title,
+            "date_posted": auction_.date_posted,
+            "date_end": auction_.date_end,
+            "description": auction_.description,
+            "buy_now_price": auction_.buy_now_price,
+            "call_price": auction_.call_price,
+            "image": auction_.image,
+            "user_id": auction_.user_id,
+        }
+    )
+
+
+@app.route("/api/auctions/all", methods=["GET"])
+def api_all():
+    return jsonify(auction_list)
+
+
+@app.route("/api/auctions", methods=["GET"])
+def api_id():
+    if "id" in request.args:
+        id = int(request.args["id"])
+        if id > auction_count or id <= 0:
+            return "Error: Invalid id.\n"
+    else:
+        return "Error: No id field provided. Please specify an id.\n"
+
+    results = []
+    for auction in auction_list:
+        if auction["id"] == id:
+            results.append(auction)
+
+    return jsonify(results)
